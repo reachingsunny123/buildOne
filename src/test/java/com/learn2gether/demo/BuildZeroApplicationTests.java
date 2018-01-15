e95e2f34c969f9ab6bf7491eceaeaf68386da6d5
Anirban Mukherjee <reachingsunny123@gmail.com>
2018-01-15 22:33:18 +0530

    First commit

---
+++ src/test/java/com/learn2gether/demo/BuildZeroApplicationTests.java
@@ -0,0 +1,21 @@
+package com.learn2gether.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.learn2gether.BuildOneApplication;
+
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = BuildZeroApplication.class)
+@WebAppConfiguration
+public class BuildZeroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
